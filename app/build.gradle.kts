770de6fe49c8b8bd772320dbe4f6d94ec5e62c04
Linka <Polina153@mail.ru>
2024-05-18 15:50:16 +0400

    Parcelize Parcelable onRestoreInstanceState

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-parcelize")
 }
+
 
 android {
     namespace = "com.example.counterbutton"
--- app/src/main/java/com/example/counterbutton/MainActivity.kt
+++ app/src/main/java/com/example/counterbutton/MainActivity.kt
@@ -1,25 +1,62 @@
 package com.example.counterbutton
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button1:Button
-    private lateinit var button2:Button
-    private lateinit var button3:Button
-    private lateinit var button4:Button
 
-    private lateinit var counterTextView1:TextView
-    private lateinit var counterTextView2:TextView
-    private lateinit var counterTextView3:TextView
-    private lateinit var counterTextView4:TextView
+    private lateinit var counters: Counters
+
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var button4: Button
+
+    private lateinit var counterTextView1: TextView
+    private lateinit var counterTextView2: TextView
+    private lateinit var counterTextView3: TextView
+    private lateinit var counterTextView4: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        counters = Counters()
+       /* if(savedInstanceState == null){
+            counters = Counters()
+        }else
+            counters = savedInstanceState.getParcelable("counters")!!*/
+        initView()
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable("counters", counters)
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle
+    ) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState!=null) {
+            counters = savedInstanceState.getParcelable("counters")!!
+        }
+        setTextCounters()
+    }
+
+    private fun setTextCounters() {
+        counterTextView1.text = counters.counter1.toString()
+        counterTextView2.text = counters.counter2.toString()
+        counterTextView3.text = counters.counter3.toString()
+        counterTextView4.text = counters.counter4.toString()
+    }
+
+
+    private fun initView() {
         button1 = findViewById(R.id.button1)
         button2 = findViewById(R.id.button2)
         button3 = findViewById(R.id.button3)
@@ -31,29 +68,24 @@ class MainActivity : AppCompatActivity() {
         counterTextView3 = findViewById(R.id.counter3)
         counterTextView4 = findViewById(R.id.counter4)
 
+        //setTextCounters()
 
         button1.setOnClickListener {
-            onTap(counterTextView1)
+            counters.incrementCounter1()
+            counterTextView1.text = counters.counter1.toString()
         }
         button2.setOnClickListener {
-            onTap(counterTextView2)
+            counters.incrementCounter2()
+            counterTextView2.text = counters.counter2.toString()
         }
         button3.setOnClickListener {
-            onTap(counterTextView3)
+            counters.incrementCounter3()
+            counterTextView3.text = counters.counter3.toString()
         }
         button4.setOnClickListener {
-            onTap(counterTextView4)
+            counters.incrementCounter4()
+            counterTextView4.text = counters.counter4.toString()
         }
-
-    }
-
-    private fun onTap(text:TextView){
-
-        val pushString = text.text.toString()
-        var pushInt = pushString.toInt()
-        pushInt++
-        text.setText(pushInt.toString())
-
     }
 }
 
